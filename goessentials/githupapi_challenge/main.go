6e43f093da7810f4fc8b87273b686321b4e1b1c3
pradeepnnv <pradeepnnv@gmail.com>
2020-08-28 11:50:04 -0500

    github api challenge

---
+++ goessentials/githupapi_challenge/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+)
+
+type User struct {
+	PublicRepos int    `json:"public_repos"`
+	Name        string `json:"login"`
+}
+
+func main() {
+	url := "https://api.github.com/users/pradeepnnv"
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "githubapi_challenge: %s", err)
+	}
+	defer resp.Body.Close()
+	// fmt.Println(resp)
+	user := &User{}
+	dec := json.NewDecoder(resp.Body)
+	if err := dec.Decode(user); err != nil {
+		fmt.Fprintf(os.Stderr, "githubapi_challenge: %s", err)
+	}
+	fmt.Println(*user)
+}
