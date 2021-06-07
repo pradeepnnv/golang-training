011675b2899c05459dbc04342cc98cf75870d364
pradeepnnv <pradeepnnv@gmail.com>
2021-06-07 14:02:21 -0500

    chapter 2
    Signed-off-by: pradeepnnv <pradeepnnv@gmail.com>

---
+++ goinpractice/chapter2/multiple_handlers/multiple_handlers.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+func main() {
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/goodbye/", goodbye)
+	http.HandleFunc("/", home)
+	http.ListenAndServe(":8080", nil)
+}
+
+func hello(res http.ResponseWriter, req *http.Request) {
+	query := req.URL.Query()
+	name := query.Get("name")
+
+	if name == "" {
+		name = "Inito Montayo"
+	}
+	fmt.Fprint(res, "Hello, My name is ", name)
+}
+
+func goodbye(res http.ResponseWriter, req *http.Request) {
+	path := req.URL.Path
+	parts := strings.Split(path, "/")
+	name := parts[2]
+	if name == "" {
+		name = "Inito Montayo"
+	}
+	fmt.Fprint(res, "Good bye ", name)
+}
+
+func home(res http.ResponseWriter, req *http.Request) {
+	if req.URL.Path != "/" {
+		http.NotFound(res, req)
+		return
+	}
+	fmt.Fprint(res, "The homepage")
+}
