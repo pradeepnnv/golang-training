be5a2e19a5f0aea55403c989c4efefedac18a50c
pradeepnnv <pradeepnnv@gmail.com>
2021-08-18 21:58:58 -0500

    hello-world v5
    Signed-off-by: pradeepnnv <pradeepnnv@gmail.com>

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -2,11 +2,22 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
+const french = "French"
 const englishHelloPrefix = "Hello "
+const spanishHelloPrefix = "Hola "
+const frenchHelloPrefix = "Bonjour "
+const suffix = "!!"
 
-func Hello(name string) string {
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return fmt.Sprintf("%s%s!!", englishHelloPrefix, name)
+	if language == spanish {
+		return fmt.Sprintf("%s%s%s", spanishHelloPrefix, name, suffix)
+	}
+	if language == french {
+		return fmt.Sprintf("%s%s%s", frenchHelloPrefix, name, suffix)
+	}
+	return fmt.Sprintf("%s%s%s", englishHelloPrefix, name, suffix)
 }
--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -12,9 +12,15 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("saying hello to people", func(t *testing.T) {
-		assertCorrectMessage(t, Hello("Chris"), "Hello Chris!!")
+		assertCorrectMessage(t, Hello("Chris", ""), "Hello Chris!!")
 	})
 	t.Run(`say "Hello World!" when empty string is supplied`, func(t *testing.T) {
-		assertCorrectMessage(t, Hello(""), "Hello World!!")
+		assertCorrectMessage(t, Hello("", ""), "Hello World!!")
+	})
+	t.Run("in Spanish", func(t *testing.T) {
+		assertCorrectMessage(t, Hello("Elodie", "Spanish"), "Hola Elodie!!")
+	})
+	t.Run("in French", func(t *testing.T) {
+		assertCorrectMessage(t, Hello("Renee", "French"), "Bonjour Renee!!")
 	})
 }
