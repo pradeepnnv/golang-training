181ebeaf96ea6183b6ed273222f78badd80c19ac
pradeepnnv <pradeepnnv@gmail.com>
2021-09-01 21:43:44 -0500

    v1 dict eg
    Signed-off-by: pradeepnnv <pradeepnnv@gmail.com>

---
+++ dictionary/dictionary.go
@@ -0,0 +1,5 @@
+package dictionary
+
+func Search(d map[string]string, t string) string {
+	return d[t]
+}
---
+++ dictionary/dictionary_test.go
@@ -0,0 +1,17 @@
+package dictionary
+
+import "testing"
+
+func TestSearch(t *testing.T) {
+	dictionary := map[string]string{"test": "this is just a test"}
+	got := Search(dictionary, "test")
+	want := "this is just a test"
+	assertStrings(t, got, want)
+}
+
+func assertStrings(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q, but want %q", got, want)
+	}
+}
