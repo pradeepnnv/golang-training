41228f471477250dcb69572fab582a2a65520378
pradeepnnv <pradeepnnv@gmail.com>
2022-04-05 22:27:49 -0500

    Update1

--- documentdb/main.go
+++ documentdb/main.go
@@ -1,18 +1,33 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 
+	"github.com/cockroachdb/pebble"
+	"github.com/google/uuid"
 	"github.com/julienschmidt/httprouter"
 )
 
 type server struct {
 	port string
+	db   *pebble.DB
+}
+
+func newServer(database string, port string) (*server, error) {
+	s := server{db: nil, port: port}
+	var err error
+	s.db, err = pebble.Open(database, &pebble.Options{})
+	return &s, err
 }
 
 func main() {
-	s := server{"8080"}
+	s, err := newServer("docdb.data", "8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer s.db.Close()
 	router := httprouter.New()
 	router.POST("/docs", s.addDocument)
 	router.GET("/docs", s.searchDocuments)
@@ -24,12 +39,82 @@ func main() {
 }
 
 func (s server) addDocument(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	panic("Unimplemented")
+
+	dec := json.NewDecoder(r.Body)
+	var document map[string]any
+	err := dec.Decode(&document)
+	if err != nil {
+		jsonResponse(w, nil, err)
+		return
+	}
+
+	// New unique id for the document
+	id := uuid.New().String()
+
+	bs, err := json.Marshal(document)
+	if err != nil {
+		jsonResponse(w, nil, err)
+		return
+	}
+	err = s.db.Set([]byte(id), bs, pebble.NoSync)
+	if err != nil {
+		jsonResponse(w, nil, err)
+		return
+	}
+
+	jsonResponse(w, map[string]any{
+		"id": id,
+	}, nil)
 }
 
 func (s server) searchDocuments(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	panic("Unimplemented")
 }
-func (s server) getDocument(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	panic("Unimplemented")
+func (s server) getDocumentById(id []byte) (map[string]any, error) {
+	valBytes, closer, err := s.db.Get(id)
+
+	if err != nil {
+		return nil, err
+	}
+	defer closer.Close()
+
+	var document map[string]any
+	err = json.Unmarshal(valBytes, &document)
+	return document, err
+}
+
+func jsonResponse(w http.ResponseWriter, body map[string]any, err error) {
+	data := map[string]any{
+		"body":   body,
+		"status": "ok",
+	}
+	if err == nil {
+		w.WriteHeader(http.StatusOK)
+	} else {
+		data["status"] = "error"
+		data["error"] = err.Error()
+		w.WriteHeader(http.StatusBadRequest)
+	}
+	w.Header().Set("Content-Type", "application/json")
+
+	enc := json.NewEncoder(w)
+	err = enc.Encode(data)
+	if err != nil {
+		// TODO: setup panic handler?
+		panic(err)
+	}
+}
+
+func (s server) getDocument(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	id := ps.ByName("id")
+
+	document, err := s.getDocumentById([]byte(id))
+	if err != nil {
+		jsonResponse(w, nil, err)
+		return
+	}
+
+	jsonResponse(w, map[string]any{
+		"document": document,
+	}, nil)
 }
