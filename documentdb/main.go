a600aca11b9aaa90cc1e07999676bcd4ddc2b013
pradeepnnv <pradeepnnv@gmail.com>
2022-04-10 14:54:35 -0500

    further updates

--- documentdb/main.go
+++ documentdb/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
+	"unicode"
 
 	"github.com/cockroachdb/pebble"
 	"github.com/google/uuid"
@@ -67,9 +71,6 @@ func (s server) addDocument(w http.ResponseWriter, r *http.Request, _ httprouter
 	}, nil)
 }
 
-func (s server) searchDocuments(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	panic("Unimplemented")
-}
 func (s server) getDocumentById(id []byte) (map[string]any, error) {
 	valBytes, closer, err := s.db.Get(id)
 
@@ -117,4 +118,227 @@ func (s server) getDocument(w http.ResponseWriter, r *http.Request, ps httproute
 	jsonResponse(w, map[string]any{
 		"document": document,
 	}, nil)
+}
+
+// Handles either quoted strings or unquoted strings of only continguous digits and letters
+
+func lexString(input []rune, index int) (string, int, error) {
+	if index >= len(input) {
+		return "", index, nil
+	}
+
+	if input[index] == '"' {
+		index++
+		foundEnd := false
+		var s []rune
+
+		// TODO: handle nested quotes
+		for index < len(input) {
+			if input[index] == '"' {
+				foundEnd = true
+				break
+			}
+			s = append(s, input[index])
+			index++
+		}
+		if !foundEnd {
+			return "", index, fmt.Errorf("Expected end of quoted string")
+		}
+
+		return string(s), index + 1, nil
+	}
+
+	// if unquoted, read as much contiguous digits/letters as there are
+
+	var s []rune
+	var c rune
+
+	// TODO: someone needs to validate there's not ...
+	for index < len(input) {
+		c = input[index]
+		if !(unicode.IsLetter(c) || unicode.IsDigit(c) || c == '.') {
+			break
+		}
+		s = append(s, c)
+		index++
+	}
+	if len(s) == 0 {
+		return "", index, fmt.Errorf("no string found")
+	}
+
+	return string(s), index, nil
+
+}
+
+type queryComparision struct {
+	key   []string
+	value string
+	op    string
+}
+
+type query struct {
+	ands []queryComparision
+}
+
+// E.g. q=a.b:12
+
+func parseQuery(q string) (*query, error) {
+	if q == "" {
+		return &query{}, nil
+	}
+	i := 0
+	var parsed query
+	var qRune = []rune(q)
+	for i < len(qRune) {
+		// Eat whitespace
+		for unicode.IsSpace(qRune[i]) {
+			i++
+		}
+
+		key, nextIndex, err := lexString(qRune, i)
+
+		if err != nil {
+			return nil, fmt.Errorf("expected valid key, got [%s]: `%s`", err, q[nextIndex:])
+		}
+
+		// Expect some operator
+
+		if q[nextIndex] != ':' {
+			return nil, fmt.Errorf("expected colon at %d, got: `%s`", nextIndex, q[nextIndex:])
+		}
+
+		i = nextIndex + 1
+		op := "="
+		if q[i] == '>' || q[i] == '<' {
+			i++
+			op = string(q[i])
+		}
+
+		value, nextIndex, err := lexString(qRune, i)
+		if err != nil {
+			return nil, fmt.Errorf("expected valid value, got [%s]: `%s`", err, q[nextIndex:])
+		}
+
+		i = nextIndex
+		argument := queryComparision{key: strings.Split(key, "."), value: value, op: op}
+
+		parsed.ands = append(parsed.ands, argument)
+	}
+	return &parsed, nil
+}
+
+func (q query) match(doc map[string]any) bool {
+	for _, argument := range q.ands {
+		value, ok := getPath(doc, argument.key)
+		if !ok {
+			return false
+		}
+
+		// Handle equality
+
+		if argument.op == "=" {
+			match := fmt.Sprintf("%v", value) == argument.value
+			if !match {
+				return false
+			}
+			continue
+		}
+
+		// Handle <,>
+		right, err := strconv.ParseFloat(argument.value, 64)
+		if err != nil {
+			return false
+		}
+
+		var left float64
+		switch t := value.(type) {
+		case float64:
+			left = t
+		case float32:
+			left = float64(t)
+		case uint:
+			left = float64(t)
+		case uint8:
+			left = float64(t)
+		case uint16:
+			left = float64(t)
+		case uint32:
+			left = float64(t)
+		case uint64:
+			left = float64(t)
+		case int:
+			left = float64(t)
+		case int8:
+			left = float64(t)
+		case int16:
+			left = float64(t)
+		case int32:
+			left = float64(t)
+		case int64:
+			left = float64(t)
+		case string:
+			left, err = strconv.ParseFloat(t, 64)
+			if err != nil {
+				return false
+			}
+		default:
+			return false
+		}
+		if argument.op == ">" {
+			if left <= right {
+				return false
+			}
+			continue
+		}
+		if left >= right {
+			return false
+		}
+
+	}
+	return true
+}
+
+func getPath(doc map[string]any, parts []string) (any, bool) {
+	var docSegment any = doc
+	for _, part := range parts {
+		m, ok := docSegment.(map[string]any)
+		if !ok {
+			return nil, false
+		}
+		if docSegment, ok = m[part]; !ok {
+			return nil, false
+		}
+	}
+	return docSegment, true
+}
+
+func (s server) searchDocuments(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	q, err := parseQuery(r.URL.Query().Get("q"))
+	if err != nil {
+		jsonResponse(w, nil, err)
+		return
+	}
+	var documents []map[string]any
+
+	iter := s.db.NewIter(nil)
+	defer iter.Close()
+
+	for iter.First(); iter.Valid(); iter.Next() {
+		var document map[string]any
+		err = json.Unmarshal(iter.Value(), &document)
+		if err != nil {
+			jsonResponse(w, nil, err)
+			return
+		}
+
+		if q.match(document) {
+			documents = append(documents, map[string]any{
+				"id":   string(iter.Key()),
+				"body": document,
+			})
+		}
+
+	}
+	jsonResponse(w, map[string]any{"documents": documents,
+		"count": len(documents)}, nil)
 }
